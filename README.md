befb85a32b564dc64df3996a29379346c1a97364
inasekin <nasekinid8591@yandex.ru>
2023-04-30 19:32:31 +0300

    feat: add docker and @nestjs/config

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # API
+
+docker-compose up -d
+docker ps
+docker stop <id>
+docker start <id>
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { PageModule } from './page/page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [AuthModule, PageModule, ProductModule, ReviewModule],
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		PageModule,
+		ProductModule,
+		ReviewModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
